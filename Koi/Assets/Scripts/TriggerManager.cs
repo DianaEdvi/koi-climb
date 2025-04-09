be8f0e8427ad5d563dd4ba4262c49e6419964414
Diana <edvi.diana@gmail.com>
2025-04-10 01:15:26 +0900

    changed triggers to be the fish and not the space between them

--- Koi/Assets/Scripts/TriggerManager.cs
+++ Koi/Assets/Scripts/TriggerManager.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class TriggerManager : MonoBehaviour
 {
     private KoiProperties _koiProperties;
+    private GameObject[] _collectibles;
     
     // Start is called before the first frame update
     void Start()
     {
         _koiProperties = GameObject.FindGameObjectWithTag("Player").GetComponent<KoiProperties>();
+        _collectibles = GameObject.FindGameObjectsWithTag("Collectible");
     }
 
     // Update is called once per frame
@@ -23,12 +25,16 @@ public class TriggerManager : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Collectible"))
         {
-            Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
         }
 
-        if (other.gameObject.CompareTag("Obsticle"))
+        if (other.gameObject.CompareTag("Obstacle"))
         {
             _koiProperties.gameObject.transform.position = new Vector3(0, 0, 0);
+            foreach (var collectible in _collectibles)
+            {
+                collectible.gameObject.SetActive(true);
+            }
         }
         
     }
