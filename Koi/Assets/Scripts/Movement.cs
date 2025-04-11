84f0fadcfe7a6ac22c96dc370fa4818429965c45
Diana <edvi.diana@gmail.com>
2025-04-12 00:36:00 +0900

    got rid of dragonMovement script bcs it was pissing me off

--- Koi/Assets/Scripts/Movement.cs
+++ Koi/Assets/Scripts/Movement.cs
@@ -14,9 +14,10 @@ public class Movement : MonoBehaviour
     private Vector3 _origin; // the point to circle around
     private float _angle; // the current angle around the circle 
     [SerializeField] private float startAngle; // the start angle
-    [SerializeField] private float startRotation; // the start angle
+    // [SerializeField] private float startRotation; // the start angle
     private Koi _koi; // general properties for both koi 
-    private int _rotation = 0;
+    // private int _rotation = 0;
+    [SerializeField] private string creatureType;
     
     // Start is called before the first frame update
     void Start()
@@ -30,16 +31,36 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Perform rotation
+        // Increase angle according to speed
         _angle += _koi.SpinSpeed;
-        _position.localPosition = new Vector3(_origin.x + _koi.Radius * Mathf.Cos(_angle), _origin.y + _koi.Radius * Mathf.Sin(_angle), 0);
         
-        // Apply spin on Z rotation to stay in the circle 
-        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + _koi.SpinSpeed * Mathf.Rad2Deg);
-        
+        MovementSpecifics(creatureType);
+       
         // Move both koi upwards and move koi left and right depending on input 
         var koiPositions = _koi.transform.position;
         _koi.transform.position = new Vector3(koiPositions.x + _koi.Direction * _koi.SideSpeed, koiPositions.y + _koi.RiseSpeed, koiPositions.z);
+    }
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    private void MovementSpecifics(string creature)
+    {
+        switch (creature)
+        {
+            case "Koi":
+                // Move around in circle 
+                _position.localPosition = new Vector3(_origin.x + _koi.Radius * Mathf.Cos(_angle), _origin.y + _koi.Radius * Mathf.Sin(_angle), 0);
         
+                // Apply spin on Z rotation to stay aligned with the circle 
+                transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + _koi.SpinSpeed * Mathf.Rad2Deg);
+                break;
+            case "Dragon":
+                // Spin the dragon
+                var rotation = transform.rotation;
+                transform.rotation = Quaternion.Euler(0, 0, _angle * Mathf.Rad2Deg);
+                break;
+            default:
+                Debug.LogError("Misspelled creature type");
+                return;
+        }
     }
 }
