899eef98fe04bbf178de5aff00002b528e457e73
Diana <edvi.diana@gmail.com>
2025-04-17 17:33:48 +0900

    added ui button for changing assist level

--- Koi/Assets/Scripts/UIHub.cs
+++ Koi/Assets/Scripts/UIHub.cs
@@ -1,32 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
-/**
- * For managing UI
- */
 public class UIHub : MonoBehaviour
 {
     private Events _events;
     [SerializeField] private Canvas canvas;
-    
-    // Start is called before the first frame update
+    [SerializeField] private Color[] assistColors;
+    private Button _button;
+    private Image _image;
+    private int _counter;
+    private string _text;
+
+
     void Start()
     {
         _events = GameObject.Find("Game").GetComponent<Events>();
         _events.onEndLevel.AddListener(ReactivateUI);
-        
+        _button = GameObject.Find("Assist level button").GetComponent<Button>();
+        _image = GameObject.Find("Assist level button").GetComponent<Image>();
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        assistColors = new Color[4];
+        assistColors[0] = new Color(1f, 200f / 255f, 200f / 255f);
+        assistColors[1] = new Color(250f / 255f, 150f / 255f, 150f / 255f);
+        assistColors[2] = new Color(245f / 255f, 100f / 255f, 100f / 255f);
+        assistColors[3] = new Color(240f / 255f, 50f / 255f, 50f / 255f);
     }
 
     private void ReactivateUI()
     {
         canvas.gameObject.SetActive(true);
+    }
+
+    public void SetAssistLevel()
+    {
+        Debug.Log("ptrssed");
+        _counter++;
+
+        if (_counter == 4)
+        {
+            _counter = 0;
+        }
+
+        if (_image != null)
+        {
+            for (var i = 0; i < assistColors.Length; i++)
+            {
+                if (i == _counter)
+                {
+                   _image.color = assistColors[i];
+                   _text = "" + i;
+                   _button.GetComponentInChildren<TMP_Text>().text = _text;
+                }
+            }
+            
+        }
     }
 }
