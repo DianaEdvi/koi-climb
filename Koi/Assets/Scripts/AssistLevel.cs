cd15947af1043e3377651f0bb6702b5edb09123d
Diana <edvi.diana@gmail.com>
2025-04-18 01:02:24 +0900

    made cool level select screen

---
+++ Koi/Assets/Scripts/AssistLevel.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class AssistLevel : MonoBehaviour
+{
+    [SerializeField] private Color[] assistColors;
+    private Button _button;
+    private Image _image;
+    private int _counter;
+    private string _text;
+    private Checkpoints _checkpoints;
+
+
+    void Start()
+    {
+        _button = gameObject.GetComponent<Button>();
+        _image = gameObject.GetComponent<Image>();
+        GameObject ckpt = GameObject.Find("Checkpoints");
+        if (ckpt != null)
+        {
+            _checkpoints = ckpt.GetComponent<Checkpoints>();
+        }
+
+        assistColors = new Color[4];
+        assistColors[0] = new Color(1f, 200f / 255f, 200f / 255f);
+        assistColors[1] = new Color(250f / 255f, 150f / 255f, 150f / 255f);
+        assistColors[2] = new Color(245f / 255f, 100f / 255f, 100f / 255f);
+        assistColors[3] = new Color(240f / 255f, 50f / 255f, 50f / 255f);
+        
+        _button.onClick.AddListener(SetAssistLevel);
+    }
+    
+    public void SetAssistLevel()
+    {
+        _counter++;
+
+        if (_counter == 4)
+        {
+            _counter = 0;
+        }
+
+        if (_image != null)
+        {
+            for (var i = 0; i < assistColors.Length; i++)
+            {
+                if (i == _counter)
+                {
+                    _image.color = assistColors[i];
+                    _text = "" + i;
+                    _button.GetComponentInChildren<TMP_Text>().text = _text;
+                    if (_checkpoints != null)
+                    {
+                        _checkpoints.NumberOfCheckpoints = i;
+                    }
+                }
+            }
+            
+        }
+    }
+}
--- Koi/Assets/Scripts/UIHub.cs
+++ Koi/Assets/Scripts/UIHub.cs
@@ -9,27 +9,35 @@ public class UIHub : MonoBehaviour
 {
     private Events _events;
     [SerializeField] private Canvas canvas;
-    [SerializeField] private Color[] assistColors;
-    private Button _button;
-    private Image _image;
-    private int _counter;
-    private string _text;
-    private Checkpoints _checkpoints;
+    // [SerializeField] private Color[] assistColors;
+    // private Button _button;
+    // private Image _image;
+    // private int _counter;
+    // private string _text;
+    // private Checkpoints _checkpoints;
 
 
     void Start()
     {
-        _events = GameObject.Find("Game").GetComponent<Events>();
-        _events.onEndLevel.AddListener(ReactivateUI);
-        _button = GameObject.Find("Assist level button").GetComponent<Button>();
-        _image = GameObject.Find("Assist level button").GetComponent<Image>();
-        _checkpoints = GameObject.Find("Checkpoints").GetComponent<Checkpoints>();
+        GameObject gameObj = GameObject.Find("Game");
+        if (gameObj != null)
+        {
+            _events = gameObj.GetComponent<Events>();
+            _events.onEndLevel.AddListener(ReactivateUI);
+        }
+        // _button = GameObject.Find("Assist level button").GetComponent<Button>();
+        // _image = GameObject.Find("Assist level button").GetComponent<Image>();
+        // GameObject ckpt = GameObject.Find("Checkpoints");
+        // if (ckpt != null)
+        // {
+        //     _checkpoints = ckpt.GetComponent<Checkpoints>();
+        // }
 
-        assistColors = new Color[4];
-        assistColors[0] = new Color(1f, 200f / 255f, 200f / 255f);
-        assistColors[1] = new Color(250f / 255f, 150f / 255f, 150f / 255f);
-        assistColors[2] = new Color(245f / 255f, 100f / 255f, 100f / 255f);
-        assistColors[3] = new Color(240f / 255f, 50f / 255f, 50f / 255f);
+        // assistColors = new Color[4];
+        // assistColors[0] = new Color(1f, 200f / 255f, 200f / 255f);
+        // assistColors[1] = new Color(250f / 255f, 150f / 255f, 150f / 255f);
+        // assistColors[2] = new Color(245f / 255f, 100f / 255f, 100f / 255f);
+        // assistColors[3] = new Color(240f / 255f, 50f / 255f, 50f / 255f);
     }
 
     private void ReactivateUI()
@@ -37,28 +45,31 @@ public class UIHub : MonoBehaviour
         canvas.gameObject.SetActive(true);
     }
 
-    public void SetAssistLevel()
-    {
-        _counter++;
-
-        if (_counter == 4)
-        {
-            _counter = 0;
-        }
-
-        if (_image != null)
-        {
-            for (var i = 0; i < assistColors.Length; i++)
-            {
-                if (i == _counter)
-                {
-                   _image.color = assistColors[i];
-                   _text = "" + i;
-                   _button.GetComponentInChildren<TMP_Text>().text = _text;
-                   _checkpoints.NumberOfCheckpoints = i;
-                }
-            }
-            
-        }
-    }
+    // public void SetAssistLevel()
+    // {
+    //     _counter++;
+    //
+    //     if (_counter == 4)
+    //     {
+    //         _counter = 0;
+    //     }
+    //
+    //     if (_image != null)
+    //     {
+    //         for (var i = 0; i < assistColors.Length; i++)
+    //         {
+    //             if (i == _counter)
+    //             {
+    //                _image.color = assistColors[i];
+    //                _text = "" + i;
+    //                _button.GetComponentInChildren<TMP_Text>().text = _text;
+    //                if (_checkpoints != null)
+    //                {
+    //                    _checkpoints.NumberOfCheckpoints = i;
+    //                }
+    //             }
+    //         }
+    //         
+    //     }
+    // }
 }
